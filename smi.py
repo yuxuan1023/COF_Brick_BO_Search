27a7cca75836bd0ee0857e8acfe7e05da6ecd06f
yuxuan1023 <107922029+yuxuan1023@users.noreply.github.com>
2024-08-19 13:04:57 +0200

    Update and rename smi.py to delete

--- smi.py
+++
@@ -1,9 +0,0 @@
-import os
-from tqdm import tqdm
-filenames=os.listdir('smi_logs')
-for filename in tqdm(filenames):
-	with open('smi_logs/{}'.format(filename)) as f:
-		lines=f.readlines()
-
-	with open('smi.log','a') as f:
-		f.write(filename+'____'+lines[0]+'\n')
